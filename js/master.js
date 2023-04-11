8975a183299594f5013b7e384b08d82abbe1b36d
Daniela Neutzling <daneneutzling@gmail.com>
2023-04-11 17:19:19 -0300

    Alteração fnc calculaPrecoTotal

--- js/master.js
+++ js/master.js
@@ -1,105 +1,89 @@
-//   arredonda valor
+// Arredonda Aalor
 function arred(d, casas) {
   let aux = Math.pow(10, casas)
   return Math.floor(d * aux) / aux
 }
 
-// transforma string para float
+// Transforma String para Float
 function textToFloat(text) {
   let valor = text.replace("R$ ", "").replace(",", ".");
   return parseFloat(valor);
 }
 
-// transforma float para string
+// Transforma Float para String
 function floatToText(float) {
   let text = "R$ " + float;
   return text;
 }
 
-let totalPedido = 0;
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Calcula o Valor Total do Pedido 
 function calculaPrecoTotal(precoItem) {
-  let totalItem;
+  let totalPedido;
+
+  if (precoItem == null || typeof precoItem == "undefined" || isNaN(precoItem) == true) {
+    totalPedido = 0;
+  } else {
+    totalPedido = parseFloat(precoItem);    
+  }
+
+  // Cria uma lista com todos elementos que tem a classe 'produto'
+  let lista = Array.from(document.querySelectorAll('.produto'))
+
+  for (let i = 0; i < lista.length; i++) {   
+    let quantidade = document.querySelectorAll('.quantidade input')[i];
+    let vlQuantidade = parseFloat(quantidade.value);
+
+    let preco = document.querySelectorAll('.preco span')[i];
+    let vlPreco = parseFloat(preco.textContent);
+
+    let presente = document.querySelectorAll('.presente input')[i];
+    let vlPresente = presente.checked;
+
+    if (vlPresente == true) {
+      totalPedido = vlQuantidade * vlPreco + 5;
+    } else {
+      totalPedido = vlQuantidade * vlPreco;
+    }
+
+    console.log('Item: ' + vlPreco + '. Total: ' + totalPedido) // retorna o calculo certo (para CADA item)
+
+  }
   
-  let parPrecoItem = parseFloat(precoItem);
-  
-  let totalPedido = parPrecoItem;
-  
-
-  // Cria uma lista com todos elementos que tem a classe 'quantidade'
-  let listaQuantidades = document.querySelectorAll('.quantidade input');
-
-  // Percorren cada elemento da lista, executando uma função que recebe parametro quantidade (referencia ao próprio elemento)
-  listaQuantidades.forEach(function(quantidade) {
-    totalItem = 0;
-
-    // 'Escuta' quando o evento de 'change' acontece no elemento de 'quantidade'
-    quantidade.addEventListener('change', function(event) {
-
-      // A partir do elemento que disparou a evento, procura-se o elemento pai, onde tem a classe 'produto' e é pego o ID
-      let idProduto = event.target.closest('.produto').id;
-
-      // Pego o valor do elemento que disparou o evento
-      let quantidade = event.target.value;
-
-      // A partir do elemento que disparou a evento, procura-se o elemento pai, onde tem a classe 'produto' e pega o conteúdo do elemento com classe 'valor'
-      let valor = event.target.closest('.produto').querySelector('.valor').textContent; 
-
-      totalItem = textToFloat(quantidade) * textToFloat(valor);
-
-      // desenvolver demais cálculos
-      totalPedido = totalPedido + totalItem;
-      
-      escreveValorTotal(totalPedido);
-    });
-  });
-
-  let paraPresente = document.querySelectorAll('.presente input');
-
-  paraPresente.forEach(function(presente) {
-    presente.addEventListener('change', function(event) {
-      let idProduto = event.target.closest('.produto').id;
-      let presente = event.target.checked; 
-
-      if (presente == true) {
-        totalPedido = totalPedido + 5;
-        escreveValorTotal(totalPedido);
-
-      } else {
-        totalPedido = totalPedido - 5;
-        escreveValorTotal(totalPedido);
-      }
-
-    })
-  })
+  escreveValorTotal(totalPedido);
+  console.log('TOTAL: ' + totalPedido) // retorna o calculo certo (para CADA item)
 
 }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+
+// Escrever o Valor Total do Pedido
 function escreveValorTotal(total) {
+  let valor = document.getElementById('valor-total').value;
   let valorTotal = document.getElementById('valor-total');
-
-  if (total === undefined) {
+  
+  if (valor == null || typeof valor == "undefined" || isNaN(valor)) {
     valorTotal.innerHTML = 'R$ ' + 0;
   } else {
     valorTotal.innerHTML = 'R$ ' + total;
   }
 }
 
+// Limpa o Carrinho
 function limparCarrinho() {
   let listaQuantidades = document.querySelectorAll('.quantidade input');
 
   listaQuantidades.forEach(function(quantidade) {
     let presente = document.querySelector('.presente input');
-    
     presente.checked = false;
     quantidade.value = 0;
-
     escreveValorTotal(0);
   });
-
 }
 
+// Função para Informar ou Cancelar Novo Item
 function novoItemECancelar() {
   if (document.getElementById('novoProduto').style.display == 'none') {
     document.getElementById('novoProduto').style.display = 'block';
@@ -110,17 +94,24 @@ function novoItemECancelar() {
   }
 }
 
+// Função para Adicionar Novo Item
+function adicionarItem() {
+
+  let produto = document.getElementById('produtoNovoItem').value;
+  let preco = document.getElementById('precoNovoItem').value;
+
+  if (produto == "" || preco == 0) {
+    alert('Informe o Nome e o Preço do Produto.')
+  } else {
+    calculaPrecoTotal(preco);
+  }
+}
+
+// Função para Excluir Novo Item
 function excluirItem() {
   document.getElementById('produtoNovoItem').value = '';
   document.getElementById('precoNovoItem').value   = 0;
-
   calculaPrecoTotal(0);
-}
-
-function adicionarItem() {
-  let precoItem = document.getElementById('precoNovoItem').value;
-
-  calculaPrecoTotal(precoItem);
 }
 
 // 'DOMContentLoaded' é um evento disparado quando o HTML é totalmente carregado
